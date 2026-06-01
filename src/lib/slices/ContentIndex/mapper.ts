3402be8c6c4ffc3a38495c213c69c30e86331cb7
Amir Vered <vered@calpoly.edu>
2026-05-31 23:14:38 -0700

    added reverse chronological order sort

--- src/lib/slices/ContentIndex/mapper.ts
+++ src/lib/slices/ContentIndex/mapper.ts
@@ -1,15 +1,27 @@
 import type { Client, Content, SliceMapper } from "@prismicio/client";
 import type { ComponentProps } from "svelte";
 
-import ContentIndex from './index.svelte';
+import ContentIndex from "./index.svelte";
 
-type Context = { client: Client<Content.AllDocumentTypes> }
+type Context = { client: Client<Content.AllDocumentTypes> };
 
-const mapper: SliceMapper< Content.ContentIndexSlice, ComponentProps<ContentIndex> , Context > = async ({slice, context}) => {
-    const {client} = context;
-    const items = slice.primary.content_type === 'Blog' ? await client.getAllByType('blogpost') : await client.getAllByType('project')
+const mapper: SliceMapper<
+	Content.ContentIndexSlice,
+	ComponentProps<ContentIndex>,
+	Context
+> = async ({ slice, context }) => {
+	const { client } = context;
 
-    return {slice, items};
+	const items =
+		slice.primary.content_type === "Blog"
+			? await client.getAllByType("blogpost", {
+					orderings: [{ field: "my.blogpost.date", direction: "desc" }]
+				})
+			: await client.getAllByType("project", {
+					orderings: [{ field: "my.project.date", direction: "desc" }]
+				});
+
+	return { slice, items };
 };
 
 export default mapper;
